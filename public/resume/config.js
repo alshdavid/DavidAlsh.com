72bb2956a7fb6eaec15b16b2583df795fe06b22a
David Alsh <alshdavid@gmail.com>
2016-12-30 14:57:23 +1300

    resume

---
+++ public/resume/config.js
@@ -0,0 +1,1 @@
+IDRViewer.config = {"pagecount":2,"title":"David Al CV","author":"David","fileName":"David_Al___Resume___Nov_2016.pdf","bounds":[[935,1210],[935,1210]],"bookmarks":[],"thumbnailType":"jpg","pageType":"html","pageLabels":[]};
